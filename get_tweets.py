3baaaa8d6fe7ff315d09f9b39182020af84bed39
Brian Koech <brighkoech@gmail.com>
2020-11-11 13:51:46 +0800

    Created invictify triggers

--- get_tweets.py
+++ get_tweets.py
@@ -1,5 +1,4 @@
 import os
-from fastapi import FastAPI
 import time
 from dotenv import load_dotenv
 import tweepy
--- serve_app.py
+++ serve_app.py
@@ -4,12 +4,12 @@ import get_tweets
 
 app = FastAPI()
 
-@app.get("/usernametweets")
+@app.post("/usernametweets")
 def get_tweets_per_username():
     get_tweets.get_tweets_by_username("LibranTechie", 50)
     return {"Message": "Done"}
 
-@app.get("/searchtermtweets")
+@app.post("/searchtermtweets")
 def get_tweets_by_searchterm():
     get_tweets.get_tweets_by_search_term(['RejectBBI'])
     return {"Message": "Done"}
