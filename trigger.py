7176a08c677a91476c0a9e6d0c73139d26a319a5
Brian Koech <brightkoech@gmail.com>
2021-05-30 14:02:22 +0800

    Updated Ngrok Link

--- trigger.py
+++ trigger.py
@@ -3,7 +3,7 @@ import requests
 ngrok_url = 'http://895390db25c9.ngrok.io'
 
 tweets_endpoint = f'{ngrok_url}/usernametweets'
-#search_endpoint = f'{ngrok_url}/searchtermtweets'
+search_endpoint = f'{ngrok_url}/searchtermtweets'
 
 r_tweets = requests.post(tweets_endpoint, json={})
 print(r_tweets.text)
