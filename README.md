82eeb71fc807c1e290bffd3d4cf4aef2314b53fb
Brian Koech <brightkoech@gmail.com>
2021-08-30 18:04:04 +0800

    Minor Edits and Added Readme Details

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # sentimeterKE
+
+## Activate environment
+
+## Run Uvicorn - run.sh
+
+## Run Ngrok - ngrok http <port>
+
+## Add ngrok link to trigger.py
+
+## Run trigger.py
--- trigger.py
+++ trigger.py
@@ -1,11 +1,11 @@
 import requests
 
-ngrok_url = 'http://895390db25c9.ngrok.io'
+ngrok_url = 'http://c043-49-196-179-113.ngrok.io'
 
 tweets_endpoint = f'{ngrok_url}/usernametweets'
 search_endpoint = f'{ngrok_url}/searchtermtweets'
 
 r_tweets = requests.post(tweets_endpoint, json={})
 print(r_tweets.text)
-#r_terms = requests.post(search_endpoint, json={})
-#print(r_terms.text)
+r_terms = requests.post(search_endpoint, json={})
+print(r_terms.text)
